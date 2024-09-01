3190a8f61152d788e4b2896dd29e52f4ab8d3f24
johnflynn <johnflynn493@gmail.com>
2024-08-31 22:57:23 -0400

    Added koin, graphql kotlin, and sqldelight dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,8 @@ val logback_version: String by project
 plugins {
     kotlin("jvm") version "2.0.20"
     id("io.ktor.plugin") version "2.3.12"
+    id("com.expediagroup.graphql") version "8.0.0"
+    id("app.cash.sqldelight") version "2.0.2"
 }
 
 group = "com.example"
@@ -27,9 +29,22 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-config-yaml")
 
-
     implementation("com.expediagroup", "graphql-kotlin-ktor-server", "8.0.0")
+    implementation("com.expediagroup", "graphql-kotlin-schema-generator", "8.0.0")
+
+
+    implementation("io.insert-koin:koin-ktor:3.5.6")
+    implementation("io.insert-koin:koin-logger-slf4j:3.5.6")
 
     testImplementation("io.ktor:ktor-server-test-host-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
+
+sqldelight {
+    databases {
+        create("Database") {
+            packageName.set("com.example")
+            dialect("app.cash.sqldelight:postgresql-dialect:2.0.2")
+        }
+    }
 }
