38e2090822ffc462eafb858e6cee2621b0fd47fe
johnflynn <johnflynn493@gmail.com>
2024-09-03 23:34:23 -0400

    Added graphql configuration

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,12 +1,32 @@
 package com.example
 
 import com.example.plugins.*
+import com.expediagroup.graphql.server.ktor.GraphQL
 import io.ktor.server.application.*
+import org.koin.dsl.module
+import org.koin.ktor.plugin.Koin
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
+val koinModule = module {
+    single {
+        Database(JdbcSqliteDriver("jdbc:sqlite:build/databases/database.db"))
+    }
+}
+
 fun Application.module() {
+    install(Koin) {
+        modules(koinModule)
+    }
+    install(GraphQL) {
+        schema {
+            packages = listOf("com.example")
+            queries = listOf(
+//                HelloWorldQuery()
+            )
+        }
+    }
     configureRouting()
 }
--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,5 +1,7 @@
 package com.example.plugins
 
+import com.expediagroup.graphql.server.ktor.graphQLPostRoute
+import com.expediagroup.graphql.server.ktor.graphiQLRoute
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -9,5 +11,7 @@ fun Application.configureRouting() {
         get("/") {
             call.respondText("Hello World!")
         }
+        graphQLPostRoute()
+        graphiQLRoute()
     }
 }
